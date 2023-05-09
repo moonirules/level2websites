9df1eeb5fa1b320e6300582d0cf57022fa6590b8
moonirules <133018172+moonirules@users.noreply.github.com>
2023-05-09 14:19:03 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/*!
+* Start Bootstrap - Bare v5.0.3 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+$(".jumbotron").css({ height: $(window).height() + "px" });
+
+$(window).on("resize", function() {
+  $(".jumbotron").css({ height: $(window).height() + "px" });
+});
